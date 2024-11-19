4a2d9b47953b036b226bd3fdf406e5febae110df
juampi58 <juanpabloloza@gmail.com>
2024-11-19 18:46:49 +0200

    reverse unwanted changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const isProd = process.env.NODE_ENV === 'production';
+module.exports = nextConfig;
 
-module.exports = {
-  output: 'export', // Enables static export
-  distDir: 'build', // Custom output directory for gh-pages
-  images: {
-    unoptimized: true, // Disable image optimization for static exports
-  },
-  assetPrefix: isProd ? '/adventure-lodge-landing' : '', // Ensure relative paths for assets
-  basePath: isProd ? '/adventure-lodge-landing' : '', // Replace <repository-name> with your GitHub repo name
-};
